2369b4e6c5fcb15cf115b464122e38864e47df7a
W. Guilherme <ravok123@gmail.com>
2020-10-14 22:48:02 -0300

    basic props

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+interface TitleProps {
+  text: string;
+}
+
+
+function Title(props: TitleProps){
+  return <h1>{props.text}</h1>
+}
+
 
 function App() {
   return (
     <div className="App">
+
+      <Title text="Title 1" />
+      <Title text="Title 2"/>
+      <Title text="Title 3"/>
+      <Title text="Title 4"/>
       
-    <h1>Hello World</h1>
+    {/* <h1>Hello World</h1> */}
     </div>
   );
 }
