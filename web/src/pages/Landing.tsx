5992e7a9cfa627091edafe74bc56414019917dce
W. Guilherme <ravok123@gmail.com>
2020-10-14 23:52:52 -0300

    route and Link from react-router-dom

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import {FiArrowRight} from 'react-icons/fi';
 
 import '../styles/landing.css'
@@ -27,9 +28,9 @@ function Landing(){
         <span>Porciúncula</span>
       </div>
 
-    <a href="" className="enter-app">
+    <Link to="/app" className="enter-app">
       <FiArrowRight size={26} color="rgba(0,0,0,0.26)"/>
-    </a>
+    </Link>
 
     </main>
 
