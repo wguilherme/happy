9c4c2acb4419697c8d76b83baf58134417dd9739
W. Guilherme <ravok123@gmail.com>
2020-10-14 23:48:15 -0300

    create routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,45 +1,13 @@
 import React from 'react';
-import {FiArrowRight} from 'react-icons/fi';
 
-import './styles/landing.css'
+import './styles/global.css'
 
-import logoImg from './images/logo.svg'
-import landing from './images/landing.svg'
-import mapMarker from './images/map-marker.svg'
-
-
+import  Routes from './routes'
 
 
 function App() {
   return (
-    <div id="page-landing">
-
-
-
-    <div className="content-wrapper">
-    <img src={logoImg} alt="Happy"/>
-
-    <main>
-      <h1>Leve felicidade para o mundo</h1>
-      <p>Visite orfanatos e mude o dia de muitas crianças</p>
-
-      <div className="location">
-        <strong>Rio de Janeiro</strong>
-        <span>Porciúncula</span>
-      </div>
-
-    <a href="" className="enter-app">
-      <FiArrowRight size={26} color="rgba(0,0,0,0.26)"/>
-    </a>
-
-    </main>
-
-    </div>
-
-
-
-
-    </div>
+    <Routes />
   );
 }
 
